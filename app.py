4c88a132b1aeb3fe4ab83536b7dd3036db7db4b7
Exyloss <antonin.pons@protonmail.com>
2022-03-25 20:22:30 +0100

    commit

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, send_file, redirect
 from werkzeug.utils import secure_filename
 import os
-from pathpy import Path
 
 app = Flask(__name__)
 
@@ -10,24 +9,19 @@ try:
 except:
     print("Dossier déjà créé.")
 
-app.config["UPLOAD_FOLDER"] = "uploads/"
-path = Path()
+os.chdir("uploads")
+app.config["UPLOAD_FOLDER"] = os.getcwd()
 
-def list_files(d="uploads/"):
-    ls_f = os.popen("find "+d+"* -maxdepth 0 -type f | awk -F '/' '{print $NF}'").read()
-    ls_f = ls_f.split("\n")
-    ls_f.pop()
-    ls_d = os.popen("find "+d+"* -maxdepth 0 -type d | awk -F '/' '{print $NF}'").read()
-    ls_d = ls_d.split("\n")
-    ls_d.pop()
+def list_files():
+    ls_f = list(filter(os.path.isfile, os.listdir(".")))
+    print(ls_f)
+    ls_d = list(filter(os.path.isdir, os.listdir(".")))
     print(ls_d)
     return [ls_f, ls_d]
 
 @app.route('/')
 def index():
-    #path.reset()
-    #app.config["UPLOAD_FOLDER"] = path.show()
-    return render_template('index.html', files=list_files(path.show())[0], folders=list_files(path.show())[1], path=path.show())
+    return render_template('index.html', files=list_files()[0], folders=list_files()[1], path=os.getcwd())
 
 @app.route('/add', methods = ['GET', 'POST'])
 def upload_file():
@@ -35,33 +29,41 @@ def upload_file():
         files = request.files.getlist("files[]")
         for f in files:
             filename = secure_filename(f.filename)
-            f.save(app.config['UPLOAD_FOLDER'] + filename)
-            file = open(app.config['UPLOAD_FOLDER'] + filename,"r")
-            try:
+            f.save(app.config['UPLOAD_FOLDER']+"/"+filename)
+            file = open(app.config['UPLOAD_FOLDER']+"/"+filename,"rb")
+            if True:
                 content = file.read()
-                os.system("echo \""+content+"\" > "+"uploads/"+filename)
-            except:
+                #print(content)
+                #os.system("touch "+os.getcwd()+"/"+filename)
+                #os.system("echo "+content+" > "+os.getcwd()+"/"+filename)
+                file = open(os.getcwd()+"/"+filename, "wb")
+                file.write(content)
+                file.close()
+            else:
                 print("erreur lors de la lecture du fichier.")
     #return render_template('index.html', files=list_files())
     return redirect("/")
 
 @app.route('/remove/<name>')
 def remove_file(name):
-    os.system("rm -rf "+path.show()+name)
+    os.system("rm -rf "+os.getcwd()+"/"+name)
     #return render_template('index.html', files=list_files())
     return redirect("/")
 
 @app.route('/download/<name>')
 def download_file(name):
-    return send_file(path.show()+name, as_attachment = True)
+    return send_file(os.getcwd()+"/"+name, as_attachment = True)
 
 @app.route('/edit/<name>')
 def edit_file(name):
     print(name)
-    file = open(path.show()+name, "r")
-    file_content = file.read()
-    file.close()
-    return render_template('editor.html', file_content=file_content, file_name=name)
+    try:
+        file = open(os.getcwd()+"/"+name, "r")
+        file_content = file.read()
+        file.close()
+        return render_template('editor.html', file_content=file_content, file_name=name)
+    except:
+        return redirect("/")
 
 @app.route('/newfolder', methods = ["POST"])
 def new_folder():
@@ -69,7 +71,7 @@ def new_folder():
         folder_name = request.form['folder_input']
         folder_name = secure_filename(folder_name)
         try:
-            os.mkdir("uploads/"+folder_name)
+            os.mkdir(folder_name)
         except:
             print("Erreur lors de la création du dossier.")
     return redirect("/")
@@ -91,8 +93,8 @@ def search_file():
         print(search)
         res=[]
         res2=[]
-        files = list_files(path.show())[0]
-        folders = list_files(path.show())[1]
+        files = list_files()[0]
+        folders = list_files()[1]
         for i in files:
             if search in i:
                 res.append(i)
@@ -100,22 +102,25 @@ def search_file():
             if search in i:
                 res2.append(i)
         print(res)
-        return render_template('index.html', files=res, folders=res2, path=path.show())
+        return render_template('index.html', files=res, folders=res2, path=os.getcwd())
     else:
         return redirect("/")
 
 @app.route('/goto/<folder>')
 def goto_folder(folder):
     if folder in list_files()[1]:
-        path.add(folder)
-        app.config["UPLOAD_FOLDER"] = path.show()
-    #return render_template('index.html', files=list_files(path.show())[0], folders=list_files(path.show())[1])
+        try:
+            os.chdir(folder)
+            app.config["UPLOAD_FOLDER"] = os.getcwd()
+        except:
+            print("Erreur, dossier inconnu.")
     return redirect("/")
 
 @app.route('/return')
 def return_folder():
-    path.up()
-    app.config["UPLOAD_FOLDER"] = path.show()
+    if os.getcwd() != "/home/antonin/prog/projet-nsi/uploads":
+        os.chdir("..")
+        app.config["UPLOAD_FOLDER"] = os.getcwd()
     return redirect("/")
 
 if __name__ == '__main__':
